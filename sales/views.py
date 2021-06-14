08130da64d527ec01d23c14b50ed0501318e0e8a
hossainchisty <hossain.chisty11@gmail.com>
2021-06-15 00:11:55 +0600

    🎉Update views.py

--- sales/views.py
+++ sales/views.py
@@ -1,62 +1,73 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    UpdateAPIView,
+    RetrieveAPIView,
+    DestroyAPIView
+) 
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+# from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
-from .models import Product,Order
-from .serializers import ProductSerializer,OrderSerializer
+from rest_framework.throttling import ScopedRateThrottle
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.throttling import UserRateThrottle,AnonRateThrottle
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
+from .models import Product,Order
+from .throttling import ProductRateThrottle
+from .serializers import ProductSerializer,OrderSerializer
 
-class ProductAPIView(generics.ListAPIView):
-    # permission_classes = [IsAuthenticated]
+
+class ProductAPIView(ListAPIView):
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['^name']
+    throttle_classes = [ProductRateThrottle,AnonRateThrottle]
+    # ordering_fields = ['Name']
+
+class CreateProduct(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+class UpdateProduct(UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class CreateProduct(generics.CreateAPIView):
-    # permission_classes = [IsAuthenticated]
+class RetrieveProduct(RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class UpdateProduct(generics.UpdateAPIView):
-    # permission_classes = [IsAuthenticated]
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-class RetrieveProduct(generics.RetrieveAPIView):
-    # permission_classes = [IsAuthenticated]
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-class DestroyProduct(generics.DestroyAPIView):
-    # permission_classes = [IsAuthenticated]
+class DestroyProduct(DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 '''
-
 '''
-class OrderAPIView(generics.ListAPIView):
+class OrderAPIView(ListAPIView):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
     # authentication_classes = [SessionAuthentication, BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
+    # throttle_classes = [UserRateThrottle,AnonRateThrottle]
 
-class CreateOrder(generics.CreateAPIView):
-    # permission_classes = [IsAuthenticated]
+class CreateOrder(CreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    # throttle_classes = [UserRateThrottle,AnonRateThrottle]
 
-class UpdateOrder(generics.UpdateAPIView):
-    # permission_classes = [IsAuthenticated]
+class UpdateOrder(UpdateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    throttle_classes = [UserRateThrottle,AnonRateThrottle]
 
-class RetrieveOrder(generics.RetrieveAPIView):
-    # permission_classes = [IsAuthenticated]
+class RetrieveOrder(RetrieveAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    throttle_classes = [UserRateThrottle,AnonRateThrottle]
 
-class DestroyOrder(generics.DestroyAPIView):
-    # permission_classes = [IsAuthenticated]
+class DestroyOrder(DestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    throttle_classes = [UserRateThrottle,AnonRateThrottle]
