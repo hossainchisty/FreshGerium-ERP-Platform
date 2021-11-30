b729bd39a97f00a51ddfd39d0b0c8e4d35c17e44
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-11-30 20:02:25 +0600

    feat: Added suppliers model fields✨

---
+++ suppliers/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Supplier
+
+admin.site.register(Supplier)
---
+++ suppliers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SuppliersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'suppliers'
