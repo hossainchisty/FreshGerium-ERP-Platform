1cfd5b29215f133aa4ed2ce22ca6cb5a8011c3c2
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-04 16:41:51 +0600

    refactor: converted CBV🛠

--- products/views/product_list_views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def product_list(request):
-    '''
-    TODO:
-     - This will reutrn list of products
-    '''
-    return render(request, 'products/product_list.html')
---
+++ products/views/product_views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+
+class ProductListView(View):
+    def get(self, request):
+        '''
+        TODO:
+        - This will reutrn list of products
+        '''
+        return render(request, 'products/product_list.html')
