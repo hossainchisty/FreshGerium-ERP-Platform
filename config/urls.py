696e07a11064129c918b07ff616cf4ef94727349
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-05 21:09:46 +0600

    feat: added user_sessions path✨

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,16 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', include('user_sessions.urls', 'user_sessions')),
     path('', include('core.urls')),
     path('profile/', include('profiles.urls')),
+    path('purchase/', include('purchase.urls')),
+    path('expense/', include('expense.urls')),
     path('products/', include('products.urls')),
     path('suppliers/', include('suppliers.urls')),
     path('settings/', include('settings.urls')),
     path('sales/', include('sales.urls')),
+    path('stock/', include('stock.urls')),
     path('customers/', include('customers.urls')),
     path('accounts/', include('accounts.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
