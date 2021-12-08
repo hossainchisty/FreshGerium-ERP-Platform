43118ea1f4701c9110b7a11689955739925cd34b
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-08 17:33:26 +0600

    feat: added customer create logic✨

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,10 @@
 
+from customers.views.create_customer_views import CreateCustomer
 from customers.views.customer_views import CustomerList
 from django.urls import include, path
 
 urlpatterns = [
     path('', CustomerList.as_view(), name='customer_list'),
+    path('add/', CreateCustomer.as_view(), name='add_customer'),
     path('api/v1/', include('customers.api.urls.customer_urls')),
 ]
