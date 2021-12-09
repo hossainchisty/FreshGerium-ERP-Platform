af1e5608be5dfcb12b68fa61bed8a04e4bb4671f
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-09 17:20:25 +0600

    feat: Added new apps ✨

---
+++ report/models.py
@@ -0,0 +1,2 @@
+from django.db import models
+from django.utils import timezone
---
+++ return/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReturnConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'return'
