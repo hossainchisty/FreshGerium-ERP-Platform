247533b8cfbc44a3d5ab940beddde8a4d9c757cb
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-12 21:51:02 +0600

    feat: Renderd list of all expense ✨

--- expense/views/manage_expense_views.py
+++ expense/views/manage_expense_views.py
@@ -1,24 +1,29 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import View
+from expense.models import Expense
 
 
 class ManageExpense(View):
     '''
-    TODO:
-    1. Add Expense Statement
-    2. Edit Expense Statement
-    3. Delete Expense Statement
-    4. Need to add pagination[10 items per page]
+    List all Expense date expense type and amount.
     '''
     def get(self, request):
-        return render(request, 'expense/manage_expense.html')
+        expenses_list = Expense.objects.all().order_by('-id')
+        paginator = Paginator(expenses_list, 20) # Show 20 Expense list per page.
+        page_number = request.GET.get('page')
+        expenses = paginator.get_page(page_number)
+        context = {
+            'expenses': expenses
+        }
+        return render(request, 'expense/manage_expense.html', context)
 
 
 class ExpenseStatement(View):
     '''
     TODO:
     1. Need to add seach wth date range and expense type
-    2. Need to add pagination[10 items per page] / Not mandatory
+    2. Need to add pagination[20 items per page] / Not mandatory
     '''
     def get(self, request):
         return render(request, 'expense/expense_statement.html')
@@ -29,7 +34,7 @@ class ExpenseItem(View):
     TODO:
     1. Need to show the expense item action buttons[Edit, Delete]
      - CSV,pdf,print
-    2. Need to add pagination[10 items per page]
+    2. Need to add pagination[20 items per page]
     '''
     def get(self, request):
         return render(request, 'expense/expense_item.html')
