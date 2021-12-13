b567185bea768bfbdf1b6252d53483e47d69a01d
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-13 19:22:25 +0600

    feat: Renderd list of all purchase✨

--- purchase/views/manage_purchase_views.py
+++ purchase/views/manage_purchase_views.py
@@ -1,13 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import View
+from purchase.models import Purchase
 
 
 class ManagePurchase(View):
     '''
-    TODO:
-    1. Need to show the purchase item action buttons[Edit]
-     - view details
-    2. Need to add pagination[10 items per page]
+    List of all purchases
     '''
     def get(self, request):
-        return render(request, 'purchase/manage_purchase.html')
+        purchase_list = Purchase.objects.all().order_by('-id')
+        paginator = Paginator(purchase_list, 25) # Show 25 customers per page.
+        page_number = request.GET.get('page')
+        purchases = paginator.get_page(page_number)
+
+        context = {
+            'purchases': purchases
+        }
+        return render(request, 'purchase/manage_purchase.html', context)
