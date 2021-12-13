1dc86879d3d1ada58a49f9358d17bc1727f511e6
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-13 20:57:51 +0600

    fix: Added db validation null🛢

--- service/models.py
+++ service/models.py
@@ -6,7 +6,7 @@ class Service(Timestamp):
     service_name = models.CharField(max_length=100)
     charge = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
-    vat = models.DecimalField(max_digits=10, decimal_places=2, default=2.00)
+    vat = models.DecimalField(max_digits=10, decimal_places=2, default=2.00, null=True, blank=True)
 
     def __str__(self):
         '''
