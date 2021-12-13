2180d7a024a463386cb681d3c421706bddd55304
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-13 19:46:22 +0600

    feat: Added service model✨🛢

--- service/models.py
+++ service/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from utils.models.common_fields import Timestamp
 
-# Create your models here.
+
+class Service(Timestamp):
+    service_name = models.CharField(max_length=100)
+    charge = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    vat = models.DecimalField(max_digits=10, decimal_places=2, default=2.00)
+
+    def __str__(self):
+        '''
+        Return a string representation of the model object.
+        '''
+        return self.service_name
