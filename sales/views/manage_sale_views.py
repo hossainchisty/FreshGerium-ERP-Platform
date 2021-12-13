23d42c47f4dadb471e70d075252569a41632e385
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-13 19:18:25 +0600

    feat: Renderd list of all sales✨

--- sales/views/manage_sale_views.py
+++ sales/views/manage_sale_views.py
@@ -1,11 +1,18 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import View
+from sales.models import Sale
 
 
 class SalesList(View):
     def get(self, request):
-        '''
-        TODO:
-        - This will reutrn list of sales
-        '''
-        return render(request, 'sales/sales_list.html')
+        ''' List of all sales '''
+        sales_list = Sale.objects.all().order_by('-id')
+        paginator = Paginator(sales_list, 25) # Show 25 customers per page.
+        page_number = request.GET.get('page')
+        sales = paginator.get_page(page_number)
+
+        context = {
+            'sales': sales
+        }
+        return render(request, 'sales/sales_list.html', context)
