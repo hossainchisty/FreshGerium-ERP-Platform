a99504cc45fdf705baedfba53193d49992eabfa0
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-14 14:42:52 +0600

    feat: Renderd list of all products and category✨

--- products/views/product_views.py
+++ products/views/product_views.py
@@ -1,20 +1,33 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import View
+from products.models import Category, Product
 
 
 class ProductListView(View):
     def get(self, request):
-        '''
-        TODO:
-        - This will reutrn list of products
-        '''
-        return render(request, 'products/product_list.html')
+        ''' This will reutrn list of products '''
+        product_list = Product.objects.all().order_by('-id')
+
+        paginator = Paginator(product_list, 25) # Show 25 customers per page.
+        page_number = request.GET.get('page')
+        products = paginator.get_page(page_number)
+
+        context = {
+            'products': products,
+        }
+        return render(request, 'products/product_list.html', context)
 
 
 class CategoryListView(View):
     def get(self, request):
-        '''
-        TODO:
-        - This will reutrn list of category
-        '''
-        return render(request, 'products/category_list.html')
+        ''' This will reutrn list of category '''
+        category_list = Category.objects.all().order_by('-id')
+        paginator = Paginator(category_list, 25) # Show 25 customers per page.
+        page_number = request.GET.get('page')
+        category = paginator.get_page(page_number)
+
+        context = {
+            'category': category,
+        }
+        return render(request, 'products/category_list.html', context)
