f732aa1f4db2fd6dcb50eabb6fedaf024a5bd09f
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-14 15:54:38 +0600

    feat: Added purchase create view url✨

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from purchase.views.add_purchase_views import CreatePurchase
 from purchase.views.delete_purchase_views import DeletePurchase
 from purchase.views.manage_purchase_views import ManagePurchase
 
 urlpatterns = [
     path('manage/', ManagePurchase.as_view(), name='manage_purchase'),
+    path('add/', CreatePurchase.as_view(), name='add_purchase'),
     path('delete/<int:pk>/', DeletePurchase.as_view(), name='delete_purchase'),
 ]
