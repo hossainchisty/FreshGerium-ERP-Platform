e2f6d7baee05dd6d00f4f2ca4f932bb75ebc7c07
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-14 15:46:21 +0600

    feat: Added purchase create functionality✨

---
+++ purchase/views/add_purchase_views.py
@@ -0,0 +1,22 @@
+
+from django.shortcuts import redirect, render
+from django.views import View
+from purchase.forms.purchase_form import PurchaseForm
+
+
+class CreatePurchase(View):
+    '''
+    Intentionally simple parent class for all views.
+    '''
+    def get(self, request, *args, **kwargs):
+        return render(request,  'purchase/add_purchase.html', {'forms': PurchaseForm()})
+
+    def post(self, request, *args, **kwargs):
+        ''' Create a new purchase '''
+        form = PurchaseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            """Provide a redirect on GET request."""
+            return redirect('manage_purchase')
+        else:
+            return render(request, 'purchase/add_purchase.html', {'forms': form})
