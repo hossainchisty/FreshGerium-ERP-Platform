ac329b26e14d30a14b50e7e950e60b9fd8ead605
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-14 14:38:22 +0600

    feat: Added urls related to service✨

--- service/urls.py
+++ service/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from service.views.add_service_views import CreateService
+from service.views.delete_service_views import DeleteService
+from service.views.manage_service_views import ManageService
+from service.views.update_service_views import ServiceUpdate
 
-urlspatterns = [
-    
+urlpatterns = [
+    path('manage/', ManageService.as_view(), name='manage_service'),
+    path('add/', CreateService.as_view(), name='add_service'),
+    path('update/<pk>/', ServiceUpdate.as_view(), name='update_service'),
+    path('delete/<pk>/', DeleteService.as_view(), name='delete_service'),
 ]
