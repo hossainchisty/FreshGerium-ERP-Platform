9ab0f4a5b0817dd2d4394c3d51a73cae276bf178
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-14 14:40:16 +0600

    feat: Renderd list of all services from db ✨

---
+++ service/views/manage_service_views.py
@@ -0,0 +1,20 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django.views.generic import View
+from service.models import Service
+
+
+class ManageService(View):
+    '''
+    List of all services
+    '''
+    def get(self, request):
+        service_list = Service.objects.all().order_by('-id')
+        paginator = Paginator(service_list, 25) # Show 25 customers per page.
+        page_number = request.GET.get('page')
+        services = paginator.get_page(page_number)
+
+        context = {
+            'services': services
+        }
+        return render(request, 'service/manage_service.html', context)
