427a621ff990b4d3e276aa1a10ca6fd0f6ddb150
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-14 16:05:20 +0600

    feat: Added unit and category urls✨

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from products.views.product_views import ProductListView, CategoryListView
+from products.views.add_category_views import CreateCategory
+from products.views.add_unit_views import CreateUnit
+from products.views.product_views import CategoryListView, ProductListView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('catetory/list', CategoryListView.as_view(), name='category_list'),
+    path('add/catetory', CreateCategory.as_view(), name='create_category'),
+    path('add/unit', CreateUnit.as_view(), name='create_unit'),
 ]
