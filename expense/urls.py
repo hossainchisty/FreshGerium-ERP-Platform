e7a66e1ae23c117e5418a620a1e11eff9a05a435
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-16 22:41:54 +0600

    fix: change urls name✨

--- expense/urls.py
+++ expense/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('add/', CreateExpense.as_view(), name='add_expense'),
     path('items/', ExpenseItem.as_view(), name='expense_item'),
     path('statement/', ExpenseStatement.as_view(), name='expense_statement'),
-    path('download-as-csv/', DownloadExpenseCSV.as_view(), name='download_expense_csv'),
+    path('export/', DownloadExpenseCSV.as_view(), name='download_expense_csv'),
     path('view/pdf/', ViewPDF.as_view(), name='view_pdf'),
     path('pdf_download/', DownloadPDF.as_view(), name="pdf_download")
 ]
