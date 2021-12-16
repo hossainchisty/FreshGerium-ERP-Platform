74827559376c5c402b58ad594345817f17b46072
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-16 21:52:29 +0600

    feat: Added reuseable function that will generate pdf file✨

---
+++ utils/render_to_pdf.py
@@ -0,0 +1,20 @@
+from io import BytesIO
+
+from xhtml2pdf import pisa
+
+from django.http import HttpResponse
+from django.template.loader import get_template
+
+
+def generate_pdf(template_src, context_dict={}):
+    '''
+    Generates a PDF file from a template and context dictionary.
+    '''
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    else:
+        return None
