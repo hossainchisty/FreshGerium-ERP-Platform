5d38b1906ea6e436f5f88b1cf9fd79a663c15304
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-16 21:50:01 +0600

    feat: Added expense pdf view✨

---
+++ expense/views/expense_pdf_views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.views.generic import View
+from expense.models import Expense
+from utils.render_to_pdf import generate_pdf
+
+
+class ViewPDF(View):
+	def get(self, request, *args, **kwargs):
+		expense = Expense.objects.all().order_by('-id')
+		pdf = generate_pdf('expense/pdf/expense_pdf.html', {'expense': expense})
+		return HttpResponse(pdf, content_type='application/pdf')
