925ae0881df273c7567fc9369a69c95f7eaeaacf
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-17 14:06:29 +0600

    feat: Added stock data can download as csv file✨

--- stock/urls.py
+++ stock/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from stock.views.export_stock_csv_views import DownloadStockCSV
 from stock.views.stock_report_views import StockReportView
 
 urlpatterns = [
     path('report/', StockReportView.as_view(), name='stock_report'),
+    path('export/', DownloadStockCSV.as_view(), name='donwload_stock_csv'),
 ]
