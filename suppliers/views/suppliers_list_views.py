e2624c8ae56c3a0b6deb3c1bf71fcb20de525e81
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-17 13:49:46 +0600

    fix: Added paginator✨

--- suppliers/views/suppliers_list_views.py
+++ suppliers/views/suppliers_list_views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import View
 from suppliers.models import Supplier
@@ -6,7 +7,10 @@ from suppliers.models import Supplier
 class SupplierList(View):
     def get(self, request):
         '''List of suppliers'''
-        suppliers = Supplier.objects.all()
+        supplier_list = Supplier.objects.all()
+        paginator = Paginator(supplier_list, 25)
+        page_number = request.GET.get('page')
+        suppliers = paginator.get_page(page_number)
 
         context = {
             'suppliers': suppliers
