11cc14953b0fb29eda1bad9e361ec4c0b90e7731
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-17 21:19:40 +0600

    feat: Added expense update and delete functionality urls✨

--- expense/urls.py
+++ expense/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from expense.views.add_expense_views import CreateExpense
+from expense.views.delete_expense_views import DeleteExpense
 from expense.views.donwload_expense_pdf import DownloadExpensePDF
 from expense.views.expense_pdf_views import ViewExpensePDF
 from expense.views.export_expense_csv_views import DownloadExpenseCSV
 from expense.views.manage_expense_views import (
     ExpenseItem, ExpenseStatement, ManageExpense,
 )
+from expense.views.update_expense_views import UpdateExpense
 
 urlpatterns = [
     path('', ManageExpense.as_view(), name='manage_expense'),
     path('add/', CreateExpense.as_view(), name='add_expense'),
+    path('delete/<pk>', DeleteExpense.as_view(), name='delete_expense'),
+    path('update/<pk>', UpdateExpense.as_view(), name='update_expense'),
     path('items/', ExpenseItem.as_view(), name='expense_item'),
     path('statement/', ExpenseStatement.as_view(), name='expense_statement'),
     path('export/', DownloadExpenseCSV.as_view(), name='download_expense_csv'),
