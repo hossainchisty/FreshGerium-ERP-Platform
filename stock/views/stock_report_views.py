0be4a23488b8ab5987c8799210a92f1b60fc9c35
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-17 13:50:12 +0600

    fix: Added paginator✨

--- stock/views/stock_report_views.py
+++ stock/views/stock_report_views.py
@@ -8,10 +8,9 @@ class StockReportView(View):
     def get(self, request):
         '''List of stock reports'''
         stock_list = Stock.objects.all()
-        paginator = Paginator(stock_list, 25) # Show 25 customers per page.
+        paginator = Paginator(stock_list, 25)
         page_number = request.GET.get('page')
         stocks = paginator.get_page(page_number)
-
 
         context = {
             'stocks': stocks
