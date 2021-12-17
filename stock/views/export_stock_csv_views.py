d5d8d2ef53051e04b5ae4bb62fe88489d8afd36e
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-17 14:27:18 +0600

    fix: change row name✨

--- stock/views/export_stock_csv_views.py
+++ stock/views/export_stock_csv_views.py
@@ -12,7 +12,7 @@ class DownloadStockCSV(View):
     def get(self, request):
         response = HttpResponse(content_type="text/csv")
         write = csv.writer(response)
-        write.writerow(["Product Name", "Product Model", "Sale Price", "Purchase Price", "In Qnty", "Out Qnty", "Total Stock", "Stock Sale Price"])
+        write.writerow(["Product Name", "Product Model", "Sale Price", "Purchase Price", "In Qnty", "Out Qnty", "In Stock", "Sale Price"])
         for stock in Stock.objects.all():
             write.writerow([stock.product.product_name, stock.product.product_model, stock.sale_price, stock.purchase_price, stock.in_qnty, stock.out_qnty, stock.stock, stock.sale_price])
             response["Content-Disposition"] = "attachment; filename=stock-data.csv"
