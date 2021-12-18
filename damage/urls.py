4ce44c87e77c08bcbc98bef9e201dbd3f1e88887
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-19 00:05:08 +0600

    feat: Added urls for damage management✨

--- damage/urls.py
+++ damage/urls.py
@@ -1,6 +1,12 @@
+from damage.views.add_manage_views import CreateDamage
 from damage.views.damange_manage_views import DamageList
+from damage.views.delete_damage_views import DeleteDamage
+from damage.views.update_damage_views import UpdateDamage
 from django.urls import path
 
 urlpatterns = [
     path('', DamageList.as_view(), name='damage_list'),
+    path('add/', CreateDamage.as_view(), name='add_damage'),
+    path('update/<int:pk>/', UpdateDamage.as_view(), name='update_damage'),
+    path('delete/<int:pk>/', DeleteDamage.as_view(), name='delete_damage'),
 ]
