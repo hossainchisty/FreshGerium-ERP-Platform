54ead05dc733e074b1a244677ccfce18ca75926e
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-19 13:56:17 +0600

    feat: Added supplier return list✨

--- returns/views/supplier_return_list.py
+++ returns/views/supplier_return_list.py
@@ -1,7 +1,17 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import View
+from returns.models import Return
 
 
 class SupplierReturnView(View):
     def get(self, request):
-        return render(request, 'return/supplier_return.html')
+        return_list = Return.objects.all()
+        paginator = Paginator(return_list, 10)
+        page_number = request.GET.get('page')
+        returns = paginator.get_page(page_number)
+
+        context = {
+            'supplier_return': returns,
+        }
+        return render(request, 'return/supplier_return.html', context)
