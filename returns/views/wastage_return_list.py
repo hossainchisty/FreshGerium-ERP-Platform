c882ebf5e7bcb5de6957518f70608b5720ccea37
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-19 14:11:48 +0600

    feat: Added list of all wastage return info✨

--- returns/views/wastage_return_list.py
+++ returns/views/wastage_return_list.py
@@ -1,7 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import View
+from returns.models import Return
 
 
 class WastageReturnView(View):
     def get(self, request):
-        return render(request, 'return/wastage_return.html')
+        '''
+        This will reutrn list of wastage items
+        '''
+        wastage_list = Return.objects.all()
+        paginator = Paginator(wastage_list, 10)
+        page_number = request.GET.get('page')
+        wastage = paginator.get_page(page_number)
+
+        context = {
+            'wastage_return': wastage,
+        }
+        return render(request, 'return/wastage_return.html', context)
