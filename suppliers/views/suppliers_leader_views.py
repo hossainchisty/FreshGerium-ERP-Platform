5039225c94e6fe2e2ec2d667ea972b672e98d5b3
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-19 13:58:09 +0600

    feat: Added suppliers leader list✨

--- suppliers/views/suppliers_leader_views.py
+++ suppliers/views/suppliers_leader_views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import View
+from suppliers.models import Supplier
+from utils.models.common_fields import Ledger
 
 
 class SuppliersLedgerList(View):
     def get(self, request):
-        '''
-        TODO:
-        - This will reutrn suppliers ledger list
-        '''
-        return render(request, 'suppliers/suppliers_ledger_list.html')
+        ''' This will reutrn suppliers ledger list '''
+        supplier = Supplier.objects.all().order_by('-id')
+        supplier_ledger = Supplier.objects.prefetch_related('supplier_ledger')
+
+        context = {
+            'suppliers': supplier_ledger,
+            'supplier_ledgers': supplier_ledger
+        }
+        return render(request, 'suppliers/suppliers_ledger_list.html', context)
