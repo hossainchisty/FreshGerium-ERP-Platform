a23c490d190e561cd3e8d79fc302ddc902427150
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-21 21:48:07 +0600

    fix: Added ```intcomma``` in total expense✨

--- expense/templatetags/today_expense.py
+++ expense/templatetags/today_expense.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from django import template
+from django.contrib.humanize.templatetags.humanize import intcomma
 from django.db.models import Sum
 from expense.models import Expense
 
@@ -9,7 +10,5 @@ register = template.Library()
 
 @register.simple_tag
 def get_today_expsense(default=0.00):
-    '''
-    This method is used to get today total expense.
-    '''
-    return Expense.objects.filter(date=date.today()).aggregate(Sum('amount'))['amount__sum'] or default
+    ''' Calculate the today total expense. '''
+    return intcomma(Expense.objects.filter(date=date.today()).aggregate(Sum('amount'))['amount__sum'] or default)
