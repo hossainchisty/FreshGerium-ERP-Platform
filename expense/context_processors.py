069f6cf61a6c90b85957872a1bf1de4ce34ca29f
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-22 01:06:04 +0600

    refactor: Added descriptive comments✨

--- expense/context_processors.py
+++ expense/context_processors.py
@@ -9,25 +9,18 @@ current = datetime.datetime.now(pytz.timezone('Asia/Dhaka'))
 
 
 def get_total_expsense(default=0.00):
-    '''
-    This method is used to get total expense.
-    '''
+    ''' Calculate the total expense of all expense. '''
     total_expsense = Expense.objects.aggregate(Sum('amount'))['amount__sum'] or default
     return {'total_expsense': total_expsense}
 
 
 def get_total_expsense_by_month(default=0.00):
-    '''
-    This method is used to get total expense by month.
-    '''
-    total_expsense_by_month = Expense.objects.filter(date__month=current.month, date__year=current.year).aggregate(Sum('amount'))['amount__sum'] or default
-    return {'total_expsense_by_month': total_expsense_by_month}
+    ''' Calculate the total expense by current month. '''
+    expsense_by_month = Expense.objects.filter(date__month=current.month, date__year=current.year).aggregate(Sum('amount'))['amount__sum'] or default
+    return {'total_expsense_by_month': expsense_by_month}
 
 
 def get_total_expsense_by_year(default=0.00):
-    '''
-    This method is used to get total expense by year.
-    '''
-    total_expsense_by_year = Expense.objects.filter(date__year=current.year).aggregate(Sum('amount'))['amount__sum'] or default
-
-    return {'total_expsense_by_year': total_expsense_by_year}
+    ''' Calculate the total expense by current year. '''
+    expsense_by_year = Expense.objects.filter(date__year=current.year).aggregate(Sum('amount'))['amount__sum'] or default
+    return {'total_expsense_by_year': expsense_by_year}
