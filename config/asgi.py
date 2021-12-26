e18f94bd2d375cd98acee33a3c6b96506e2ad913
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-26 17:41:24 +0600

    feat: Added channels asgi config✨

--- config/asgi.py
+++ config/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
+from sales.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            ws_urlpatterns
+        )
+    )
+})
