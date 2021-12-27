4dde263208c31133c9e9e95f42779bb66d6542ab
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-27 22:55:18 +0600

    fix: Added urls ✨

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
-from accounts.views.account_views import accounts
+from accounts.views.account_views import AccountView
+from accounts.views.bank_account_list import BankAccountList
 from django.urls import include, path
 
 urlpatterns = [
-    path('', accounts, name='accounts'),
+    path('', AccountView.as_view(), name='accounts'),
+    path('bank/accounts/', BankAccountList.as_view(), name='bank_accounts'),
     path('api/v1/', include('accounts.api.urls.accounts_urls')),
 ]
