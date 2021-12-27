f1da5baf526395b1e8ce5f86bb7e84c2001bd775
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-27 22:54:50 +0600

    fix: covert FVB✨

--- accounts/views/account_views.py
+++ accounts/views/account_views.py
@@ -1,5 +1,9 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.views.generic import View
 
 
-def accounts(request):
-    return render(request, 'accounts/account.html')
+class AccountView(View):
+    def get(self, request):
+        ''' This will return the accounts list '''
+        return render(request, 'accounts/accounts.html')
