cc3ae4efc3e7d5a03345cb82c2e587ba4b08fb71
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-28 20:04:52 +0600

    feat: Customize Admin✨

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,17 @@
+from accounts.models.account_models import Account
+from accounts.models.bank_account_model import Bank
 from django.contrib import admin
-from .models import Account
-# Register your models here.
 
+
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    list_display = ('bank_account_name', 'bank_account_number', 'account_type', 'bank_name', 'bank_short_name', 'bank_branch')
+    search_fields = ('bank_account_name', 'bank_account_number', 'account_type', 'bank_name', 'bank_short_name', 'bank_branch')
+    list_filter = ('account_type', 'bank_name', 'bank_short_name', 'bank_branch')
+
+
+@admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ('account_name','account_description','country')
-
-
-admin.site.register(Account, AccountAdmin)
+    list_display = ('account_name', 'country')
+    search_fields = ('account_name', 'country', 'organization_industry')
+    list_filter = ('organization_industry',)
