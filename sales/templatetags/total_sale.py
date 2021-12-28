c286b6d5a194ad32e652962b484905a3dbc65e58
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-28 20:08:17 +0600

    fix: comment and line break✨

--- sales/templatetags/total_sale.py
+++ sales/templatetags/total_sale.py
@@ -1,4 +1,3 @@
-
 from django import template
 from sales.models import Sale
 
@@ -7,7 +6,5 @@ register = template.Library()
 
 @register.simple_tag
 def get_total_sale(default=0.00):
-    '''
-    This method is used to calculate the total sales.
-    '''
+    ''' Calculate the total sale of all sales. '''
     return Sale.objects.all().count() or default
