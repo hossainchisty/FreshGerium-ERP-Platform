f7b854d2417819d01edd746b72ff000474845386
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-29 21:41:16 +0600

    fix: updated ```django-4.0``` docs url✨

--- accounts/models/__init__.py
+++ accounts/models/__init__.py
@@ -1,4 +1,4 @@
 # Organizing models in a package
-# https://docs.djangoproject.com/en/3.1/topics/db/models/#organizing-models-in-a-package
+# https://docs.djangoproject.com/en/4.0/topics/db/models/#organizing-models-in-a-package
 from .account_models import Account
 from .bank_account_model import Bank
