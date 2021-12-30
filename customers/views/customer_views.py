e5db83f5b4330522a99d393d4d2f078d776727ce
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-12-30 15:27:50 +0600

    fix: Added decorator that will filter via user✨

--- customers/views/customer_views.py
+++ customers/views/customer_views.py
@@ -1,18 +1,22 @@
 from customers.models import Customer
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.generic import View
+from utils.helper.decorators.filter import currentUser
 from utils.models.common_fields import Ledger
 
 
-class CustomerList(View):
+class CustomerList(LoginRequiredMixin, View):
 
+    @method_decorator(currentUser())
     def get(self, request):
         '''
         This will reutrn list of customer
         '''
         customer_list = Customer.objects.all().order_by('-id')
-        paginator = Paginator(customer_list, 25) # Show 25 customers per page.
+        paginator = Paginator(customer_list, 25)
         page_number = request.GET.get('page')
         customers = paginator.get_page(page_number)
 
@@ -23,6 +27,8 @@ class CustomerList(View):
 
 
 class CustomerLedgerList(View):
+
+    @method_decorator(currentUser())
     def get(self, request):
         '''
         This will reutrn list of customer ledger
