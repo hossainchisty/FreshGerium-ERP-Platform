b1e6c5c590422c0d236839dbdae1cb520c9c079f
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-05 00:13:00 +0600

    fix: Change encode type✨

--- utils/render_to_pdf.py
+++ utils/render_to_pdf.py
@@ -13,7 +13,7 @@ def generate_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html = template.render(context_dict)
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    pdf = pisa.pisaDocument(BytesIO(html.encode("utf-8")), result)
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     else:
