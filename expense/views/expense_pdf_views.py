ea713db740d2c7a5b954d03ad5403ccc99bcaed6
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-05 14:54:51 +0600

    feat: Added filter method that will filter via request user🛡

--- expense/views/expense_pdf_views.py
+++ expense/views/expense_pdf_views.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
 from django.views.generic import View
 from expense.models import Expense
+from utils.helper.decorators.filter import _currentUser
 from utils.render_to_pdf import generate_pdf
 
 
-class ViewExpensePDF(View):
+class ViewExpensePDF(LoginRequiredMixin, View):
+	''' This view will show pdf '''
+	@method_decorator(_currentUser())
 	def get(self, request, *args, **kwargs):
 		expense = Expense.objects.all().order_by('-id')
 		pdf = generate_pdf('expense/pdf/expense_pdf.html', {'expense': expense})
--- expense/views/manage_expense_views.py
+++ expense/views/manage_expense_views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.generic import View
 from expense.models import Expense
+from utils.helper.decorators.filter import _currentUser
 
 
-class ManageExpense(View):
+class ManageExpense(LoginRequiredMixin, View):
     '''
     List all Expense date expense type and amount.
     '''
+    @method_decorator(_currentUser())
     def get(self, request):
         expenses_list = Expense.objects.all().order_by('-id')
         paginator = Paginator(expenses_list, 20)
@@ -19,22 +23,24 @@ class ManageExpense(View):
         return render(request, 'expense/manage_expense.html', context)
 
 
-class ExpenseStatement(View):
+class ExpenseStatement(LoginRequiredMixin, View):
     '''
     TODO:
     1. Need to add seach wth date range and expense type
     2. Need to add pagination[20 items per page] / Not mandatory
     '''
+    @method_decorator(_currentUser())
     def get(self, request):
         return render(request, 'expense/expense_statement.html')
 
 
-class ExpenseItem(View):
+class ExpenseItem(LoginRequiredMixin, View):
     '''
     Expense item action buttons[Edit, Delete]
      - CSV,pdf,print
     TODO: 2. Need to add pagination[20 items per page]
     '''
+    @method_decorator(_currentUser())
     def get(self, request):
         expenses_list = Expense.objects.all().order_by('-id')
         paginator = Paginator(expenses_list, 20)
