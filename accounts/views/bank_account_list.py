7e056d5345864abfd09a171829d58679c1d72002
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-05 14:55:32 +0600

    feat: Added filter method that will filter via per request🛡

--- accounts/views/bank_account_list.py
+++ accounts/views/bank_account_list.py
@@ -4,12 +4,12 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
-from utils.helper.decorators.filter import currentUser
+from utils.helper.decorators.filter import _currentUser
 
 
 class BankAccountList(LoginRequiredMixin, View):
 
-    @method_decorator(currentUser())
+    @method_decorator(_currentUser())
     def get(self, request, *args, **kwarg):
         ''' This will reutrn list of bank accounts '''
         bank_account_list = Bank.objects.all().order_by('-id')
