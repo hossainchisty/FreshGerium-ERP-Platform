7bcdb35d94afc748d20666dd7d5e96dbf1213d13
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-06 21:10:49 +0600

    feat: Download damage data as csv file✔✨

--- damage/urls.py
+++ damage/urls.py
@@ -1,6 +1,7 @@
 from damage.views.add_manage_views import CreateDamage
 from damage.views.damange_manage_views import DamageList
 from damage.views.delete_damage_views import DeleteDamage
+from damage.views.export_damage_csv_views import DownloadDamageCSV
 from damage.views.update_damage_views import UpdateDamage
 from django.urls import path
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('add/', CreateDamage.as_view(), name='add_damage'),
     path('update/<int:pk>/', UpdateDamage.as_view(), name='update_damage'),
     path('delete/<int:pk>/', DeleteDamage.as_view(), name='delete_damage'),
+    path('export/', DownloadDamageCSV.as_view(), name='download_damage_csv'),
 ]
---
+++ damage/views/export_damage_csv_views.py
@@ -0,0 +1,24 @@
+import csv
+
+from damage.models import Damage
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.generic import View
+from utils.helper.decorators.filter import _currentUser
+
+
+class DownloadDamageCSV(LoginRequiredMixin, View):
+    '''
+    Automaticly download damage data as CSV file.
+    '''
+    @method_decorator(_currentUser())
+    def get(self, request):
+        response = HttpResponse(content_type="text/csv")
+        write = csv.writer(response)
+        write.writerow(["Product", "Customer", "Supplier", "Damaged Date", "Damaged Reason"])
+
+        for items in Damage.objects.all():
+            write.writerow([items.product, items.customer, items.supplier, items.damaged_date, items.damaged_reason])
+            response["Content-Disposition"] = "attachment; filename=Damage-data.csv"
+            return response
