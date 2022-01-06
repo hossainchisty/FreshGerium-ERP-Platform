5c7a7fbfb395637099e54ecd7c0a1481e1fbcfb1
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-06 21:17:53 +0600

    feat: Added damage pdf view logic♻

--- damage/urls.py
+++ damage/urls.py
@@ -1,4 +1,5 @@
 from damage.views.add_manage_views import CreateDamage
+from damage.views.damage_pdf_views import ViewDamagePDF
 from damage.views.damange_manage_views import DamageList
 from damage.views.delete_damage_views import DeleteDamage
 from damage.views.export_damage_csv_views import DownloadDamageCSV
@@ -11,4 +12,5 @@ urlpatterns = [
     path('update/<int:pk>/', UpdateDamage.as_view(), name='update_damage'),
     path('delete/<int:pk>/', DeleteDamage.as_view(), name='delete_damage'),
     path('export/', DownloadDamageCSV.as_view(), name='download_damage_csv'),
+    path('pdf/', ViewDamagePDF.as_view(), name='view_damage_pdf'),
 ]
---
+++ damage/views/damage_pdf_views.py
@@ -0,0 +1,18 @@
+from damage.models import Damage
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.generic import View
+from utils.helper.decorators.filter import _currentUser
+from utils.render_to_pdf import generate_pdf
+
+
+class ViewDamagePDF(LoginRequiredMixin, View):
+	'''
+	View damage data as PDF file.
+	'''
+	@method_decorator(_currentUser())
+	def get(self, request, *args, **kwargs):
+		damage = Damage.objects.all().order_by('-id')
+		pdf = generate_pdf('damage/pdf/damage_pdf.html', {'damage': damage})
+		return HttpResponse(pdf, content_type='application/pdf')
