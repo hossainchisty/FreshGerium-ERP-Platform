6991f222e819c08613e9d75c93e47eb3ecf3c9f6
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-12 18:35:52 +0600

    chore: change app label orders✨

--- config/urls.py
+++ config/urls.py
@@ -11,22 +11,23 @@ from django.urls import include, path
 if not settings.DEBUG:
     admin.site.__class__ = OTPAdminSite
 '''
+admin.site.site_header = 'Freshdesk CRM Platform'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('customers/', include('customers.urls')),
     path('profile/', include('profiles.urls')),
-    path('service/', include('service.urls')),
     path('purchase/', include('purchase.urls')),
-    path('expense/', include('expense.urls')),
     path('products/', include('products.urls')),
+    path('service/', include('service.urls')),
     path('suppliers/', include('suppliers.urls')),
     path('settings/', include('settings.urls')),
     path('sales/', include('sales.urls')),
+    path('expense/', include('expense.urls')),
     path('return/', include('returns.urls')),
     path('damage/', include('damage.urls')),
     path('stock/', include('stock.urls')),
-    path('customers/', include('customers.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts-user/', include('allauth.urls')),
 ]
