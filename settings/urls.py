d32a2feb51b00b20c1b60e52a68b37b472eecb0c
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-15 02:05:17 +0600

    feat: Added user change their password by entering their old password🔐

--- settings/urls.py
+++ settings/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from settings.views import settings_views as views
+from settings.views.password_change_views import passwordChangeView
+from settings.views.settings_views import settings
 
 urlpatterns = [
-    path('', views.settings, name='settings'),
+    path('', settings, name='settings'),
+    path('password/change/', passwordChangeView, name='password_change'),
 ]
---
+++ settings/views/password_change_views.py
@@ -0,0 +1,26 @@
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import redirect, render
+
+
+@login_required
+def passwordChangeView(request):
+    """ A form that lets a user change their password by entering their old
+    password.
+    """
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PasswordChangeForm(user=request.user, data=request.POST)
+            if form.is_valid():
+                form.save()
+                # update the session hash
+                update_session_auth_hash(request, form.user)
+                messages.success(request, 'Password Change Successfully!')
+                return redirect('/')
+        else:
+            form = PasswordChangeForm(user=request.user)
+        return render(request, 'settings/password_change.html', {'form': form})
+    else:
+        return redirect('login')
