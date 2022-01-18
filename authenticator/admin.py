f15106607a680cb4fb884c16edb3a2c4d6dbe1ad
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-18 17:55:03 +0600

    feat: Added custome user model to admin ✨

---
+++ authenticator/admin.py
@@ -0,0 +1,14 @@
+from authenticator.models import User
+from django.contrib import admin
+from django.contrib.auth.models import Group
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_staff', 'is_active', 'is_verified')
+    list_filter = ('is_staff', 'is_active', 'is_verified')
+    search_fields = ('email', )
+    ordering = ('email',)
+
+
+admin.site.unregister(Group)
