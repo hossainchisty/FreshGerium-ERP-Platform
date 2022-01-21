c81f9bf65283220348973b7817941dfff955fb3b
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-21 20:40:46 +0600

    feat: Added custome user model 🛢

--- utils/models/common_fields.py
+++ utils/models/common_fields.py
@@ -1,3 +1,4 @@
+from authenticator.models import User
 from django.db import models
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
@@ -5,7 +6,8 @@ from utils import random
 
 
 class Timestamp(models.Model):
-    """ Abstract base model for create and update timestamp """
+    """ Abstract base model for create and update timestamp and user for all models. """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('User'), null=True, blank=True)
     created_at = models.DateTimeField(default=now, editable=False)
     updated_at = models.DateTimeField(default=now, editable=False)
 
