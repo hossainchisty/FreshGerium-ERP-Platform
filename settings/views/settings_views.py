2a1469413bd8d8772aeaa630048e268c2e73b2f1
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-21 20:38:11 +0600

    feat: User can update there account info✨

--- settings/views/settings_views.py
+++ settings/views/settings_views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from settings.forms.user_settings_form import UserSettingsForm
 
 
 def settings(request):
-    return render(request, 'settings/settings.html')
+    user = request.user
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        owner_name = request.POST.get('owner_name')
+        mobile_number = request.POST.get('mobile_number')
+        organization_name = request.POST.get('organization_name')
+        business = request.POST.get('business')
+        business_manager_name = request.POST.get('business_manager_name')
+        brand_logo = request.FILES.get('brand_logo_file')
+
+        user.email = email
+        user.owner_name = owner_name
+        user.mobile_number = mobile_number
+        user.organization_name = organization_name
+        user.business = business
+        user.business_manager_name = business_manager_name
+        user.is_verified = True
+        user.brand_logo = brand_logo
+        user.save()
+        messages.success(request, 'Settings updated successfully')
+        return redirect('settings')
+    else:
+        return render(request, 'settings/settings.html', {'form': UserSettingsForm()})
