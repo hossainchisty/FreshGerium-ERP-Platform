ff2dcadcfa06ef51a2a79357e2faaf97b2240e52
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-21 20:39:08 +0600

    feat: Added signal ready method ✨

--- settings/apps.py
+++ settings/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SettingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'settings'
+    varbose_name = 'User Settings'
+
+    def ready(self):
+        import settings.signals
