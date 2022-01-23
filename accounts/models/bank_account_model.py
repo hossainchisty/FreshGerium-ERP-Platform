684314d8cb4e3869f6b75d8cf035290ae968c47f
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-23 16:16:27 +0600

    chore: separate account type tuple and writen comments✨

--- accounts/models/bank_account_model.py
+++ accounts/models/bank_account_model.py
@@ -1,22 +1,21 @@
+from common.utils import ACCOUNT_TYPE
 from django.db import models
 from utils.models.common_fields import Timestamp
 
 
 class Bank(Timestamp):
+    '''Model definition for Bank.'''
+
     bank_account_name = models.CharField(verbose_name='Bank Account Name', max_length=25)
     bank_account_number = models.IntegerField(verbose_name='Bank Account Number', unique=True)
-    ACCOUNT_TYPE = (
-        ('savings account', 'Savings Account'),
-        ('current account', 'Current Account'),
-        ('fixed Deposit account', 'Fixed Deposit Account'),
-        ('recurring Deposit account', 'Recurring Deposit Account'),
-    )
+
     account_type = models.CharField(verbose_name='Bank Account Type', max_length=225, choices=ACCOUNT_TYPE)
     bank_name = models.CharField(verbose_name='Bank Name', max_length=25)
     bank_short_name = models.CharField(verbose_name='Bank Short Name', max_length=5, null=True, blank=True)
     bank_branch = models.CharField(verbose_name='Bank Branch', max_length=25)
 
     class Meta:
+        '''Meta definition for Bank.'''
         verbose_name = 'Bank Account'
         verbose_name_plural = 'Bank Accounts'
 
