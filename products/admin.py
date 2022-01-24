b792b5feeb0b6233c0f6ce2058826837463137f5
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-25 02:04:19 +0600

    🚸 Customize admin with product, category, unit models

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,26 @@
+from import_export.admin import ImportExportModelAdmin
+
 from django.contrib import admin
 
-from .models import Category, Product
+from .models import Category, Product, Unit
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(ImportExportModelAdmin):
+    list_display = ('product_name', 'product_code', 'product_model','price', 'supplier_price', 'unit', 'category', 'supplier')
+    list_filter = ('category', 'unit')
+    search_fields = ('name', 'category__name', 'unit__name')
+
+
+@admin.register(Category)
+class CategoryAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('name',)
+
+
+@admin.register(Unit)
+class UnitAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'status')
+    list_filter = ('status',)
+    search_fields = ('name',)
