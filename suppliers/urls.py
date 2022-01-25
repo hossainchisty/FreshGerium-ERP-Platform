8263b69a7c5c33542fa3a035de9ed30b1d36d126
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-25 19:08:27 +0600

    ✨ Added supplier create url

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from suppliers.views.add_supplier_views import AddSupplier
 from suppliers.views.suppliers_leader_views import SuppliersLedgerList
 from suppliers.views.suppliers_list_views import SupplierList
 
 urlpatterns = [
     path('', SupplierList.as_view(), name='suppliers_list'),
+    path('add/', AddSupplier.as_view(), name='suppliers_add'),
     path('ledger', SuppliersLedgerList.as_view(), name='suppliers_ledger_list'),
 ]
