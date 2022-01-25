972b6ead619158a7437145047328d10af142a4d0
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-01-25 19:08:08 +0600

    ✨ Added supplier create views

---
+++ suppliers/views/add_supplier_views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, render
+from django.views.generic import View
+from suppliers.forms.suppliers_form import SupplierForm
+
+
+class AddSupplier(LoginRequiredMixin, View):
+    '''
+    Intentionally simple parent class for all views.
+    '''
+
+    def get(self, request):
+        ''' Get the suppliers form '''
+        return render(request, 'suppliers/add_suppliers.html', {'form': SupplierForm()})
+
+    def post(self, request, *args, **kwargs):
+        ''' Create a new suppliers '''
+        form = SupplierForm(request.POST)
+        if form.is_valid():
+            form.save()
+            '''Provide a redirect on GET request.'''
+            return redirect('suppliers_list')
+        else:
+            return render(request, 'suppliers/add_suppliers.html', {'form': form})
