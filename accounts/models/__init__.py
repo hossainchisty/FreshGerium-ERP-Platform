dfb46b413616beddb17a7df317c9aa829d0f0b2c
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-05 15:37:53 +0600

    feat; Added author name

--- accounts/models/__init__.py
+++ accounts/models/__init__.py
@@ -2,3 +2,5 @@
 # https://docs.djangoproject.com/en/4.0/topics/db/models/#organizing-models-in-a-package
 from .account_models import Account
 from .bank_account_model import Bank
+
+__author__ = "Hossain Chisty"
