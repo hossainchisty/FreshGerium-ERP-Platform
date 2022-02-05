ac55d6bcd1520687b3bed9be7b529ae691dd7239
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-05 20:10:11 +0600

    ✨ New category model

--- expense/models.py
+++ expense/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from utils.models.common_fields import Timestamp
 
 
+class Category(Timestamp):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
 class Expense(Timestamp):
     """
     Expense model for storing expense data🛢
@@ -9,6 +20,7 @@ class Expense(Timestamp):
     date = models.DateField()
     expense_type = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f"{self.expense_type} - {self.amount}"
