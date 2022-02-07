acc5d013298e8c7f297cb0e47d9b6937516b24d5
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-07 17:39:14 +0600

    ✨Added urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
-from . import views
+from profiles.views.data_download_views import data_download
+from profiles.views.password_verification_views import password_verification
+from profiles.views.profile_views import profile
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
+    path('', profile, name='profile'),
+    path('download/request/', data_download, name='data_download'),
+    path('verification/', password_verification, name='password_verification'),
 ]
