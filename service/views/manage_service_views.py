2add9ded7c4883460847adb3a51608049638ea9c
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-08 20:07:51 +0600

    ⚡️ Added per view cache for 3min

--- service/views/manage_service_views.py
+++ service/views/manage_service_views.py
@@ -1,13 +1,18 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.views.generic import View
 from service.models import Service
+from utils.helper.decorators.filter import _currentUser
 
 
+@method_decorator(cache_page(60 * 3), name='dispatch')
 class ManageService(View):
     '''
     List of all services
     '''
+    @method_decorator(_currentUser())
     def get(self, request):
         service_list = Service.objects.all().order_by('-id')
         paginator = Paginator(service_list, 25)
