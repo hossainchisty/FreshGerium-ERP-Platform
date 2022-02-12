5acf12e7469f9ee4eae982c468588d5ff020ad4d
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-12 13:08:23 +0600

    ♻️ Change verbose name for admin

--- authenticator/apps.py
+++ authenticator/apps.py
@@ -4,6 +4,8 @@ from django.apps import AppConfig
 class AuthenticatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authenticator'
+    verbose_name = 'User Accounts'
+    verbose_name_plural = 'User Accounts'
 
     def ready(self):
         import authenticator.signals
