0ee66bf7da41086df2a3c6cb155e4a9cf4659fb2
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-17 15:11:06 +0600

    🎨 Added superuser status in admin

--- authenticator/admin.py
+++ authenticator/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import Group
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'is_staff', 'is_active', 'is_verified')
+    list_display = ('email', 'is_superuser', 'is_staff', 'is_active', 'is_verified')
     list_filter = ('is_staff', 'is_active', 'is_verified')
     search_fields = ('email', )
     ordering = ('email',)
