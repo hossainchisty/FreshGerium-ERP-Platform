15857f63f6389626807f50be025d13f5fa297247
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-17 15:09:10 +0600

    🐛 Fixed ```is_staff``` row set to true

--- authenticator/manager.py
+++ authenticator/manager.py
@@ -23,6 +23,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
