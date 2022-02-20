dab07a5ab5535bb72c22cc603710b01c56602f18
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-20 15:16:41 +0600

    ✨ Added new fields

--- returns/models.py
+++ returns/models.py
@@ -16,7 +16,6 @@ class Return(models.Model):
     purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE, null=True, blank=True)
     reutrn_date = models.DateField(default=timezone.now)
     additional_information = models.TextField(max_length=500, help_text="e.g. My phone has missing headphones", verbose_name="Additional Information (optional)", blank=True, null=True)
-    is_returned = models.BooleanField(default=False)
     select_a_reason = [
         ('item is defective or not working', 'Item is defective or not working'),
         ('item or accessory is missing in the', 'Item or accessory is missing in the'),
@@ -29,6 +28,14 @@ class Return(models.Model):
         ('item is damaged/broken/has dent or scratches', 'Item is damaged/broken/has dent or scratches'),
     ]
     return_reason = models.CharField(max_length=100, choices=select_a_reason)
+    is_returned = models.BooleanField(default=False)
+    is_returned_by_customer = models.BooleanField(default=False)
+    is_returned_by_supplier = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.product.product_name} - {self.return_reason}'
+        if self.is_returned_by_customer:
+            return f'Customer {self.customer} return {self.product} due to {self.return_reason}'
+        elif self.is_returned_by_supplier:
+            return f'Supplier {self.supplier} return {self.product} due to {self.return_reason}'
+        else:
+            return f'{self.product} returned due to this {self.return_reason}'
