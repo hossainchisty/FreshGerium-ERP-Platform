5e9dcb8a7a7053f1715e851767e05598887634fc
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-21 14:31:36 +0600

    🎨 Design Service Admin

--- service/admin.py
+++ service/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from service.models import Service
 
-admin.site.register(Service)
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'service_name', 'description', 'charge')
+    search_fields = ('service_name',)
+    list_filter = ('customer',)
+    ordering = ('customer',)
+    
+admin.site.register(Service, ServiceAdmin)
