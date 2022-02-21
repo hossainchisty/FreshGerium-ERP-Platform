543c3f1b7c6a82a6608411a0e7626eba0f6b099b
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-21 14:42:45 +0600

    ✨ Added category lists

--- expense/urls.py
+++ expense/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from expense.views.add_category_views import CreateCategory
 from expense.views.add_expense_views import CreateExpense
 from expense.views.delete_expense_views import DeleteExpense
 from expense.views.download_expense_pdf import DownloadExpensePDF
@@ -6,16 +7,18 @@ from expense.views.expense_pdf_views import ViewExpensePDF
 from expense.views.export_expense_csv_views import DownloadExpenseCSV
 from expense.views.export_expense_excle_views import DownloadExpenseEXCLE
 from expense.views.manage_expense_views import (
-    ExpenseItem, ExpenseStatement, ManageExpense,
+    ExpenseCategory, ExpenseItem, ExpenseStatement, ManageExpense,
 )
 from expense.views.update_expense_views import UpdateExpense
 
 urlpatterns = [
     path('', ManageExpense.as_view(), name='manage_expense'),
     path('add/', CreateExpense.as_view(), name='add_expense'),
+    path('add/category/', CreateCategory.as_view(), name='add_category'),
     path('delete/<pk>', DeleteExpense.as_view(), name='delete_expense'),
     path('update/<pk>', UpdateExpense.as_view(), name='update_expense'),
     path('items/', ExpenseItem.as_view(), name='expense_item'),
+    path('category/', ExpenseCategory.as_view(), name='expense_category'),
     path('statement/', ExpenseStatement.as_view(), name='expense_statement'),
     path('export/csv/', DownloadExpenseCSV.as_view(), name='download_expense_csv'),
     path('export/excle/', DownloadExpenseEXCLE.as_view(), name='download_expense_excle'),
--- expense/views/add_expense_views.py
+++ expense/views/add_expense_views.py
@@ -1,7 +1,6 @@
-
 from django.shortcuts import redirect, render
 from django.views import View
-from expense.models import Expense
+from expense.models import Category, Expense
 
 
 class CreateExpense(View):
@@ -9,14 +8,18 @@ class CreateExpense(View):
     Intentionally simple parent class for all views.
     '''
     def get(self, request, *args, **kwargs):
-        return render(request,  'expense/add_expense.html')
+        ''' Get the expense form '''
+        categories = Category.objects.all()
+        return render(request,  'expense/add_expense.html', {'categories': categories})
 
     def post(self, request, *args, **kwargs):
         ''' Create a new expense '''
         expense_date = request.POST.get('expense_date')
         expense_type = request.POST.get('expense_type')
         expense_amount = request.POST.get('expense_amount')
-        expense = Expense(date=expense_date, expense_type=expense_type, amount=expense_amount)
+        categories = request.POST.get('categories')
+        expense = Expense(date=expense_date, expense_type=expense_type, amount=expense_amount, category=categories)
+
         expense.save()
         """Provide a redirect on GET request."""
         return redirect('manage_expense')
