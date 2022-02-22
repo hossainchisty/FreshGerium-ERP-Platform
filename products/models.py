ce65c1fd8f0ae2650a4d20a2bba6ab07e7e9a3c1
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-22 17:01:33 +0600

    ♻️ Improved product model fields

--- products/models.py
+++ products/models.py
@@ -1,3 +1,6 @@
+from cloudinary.models import CloudinaryField
+from simple_history.models import HistoricalRecords
+
 from django.db import models
 from suppliers.models import Supplier
 from utils import random
@@ -19,20 +22,41 @@ class Category(models.Model):
 
 
 class Product(Timestamp):
-    """
-    Product model for storing product data🛢
-    """
+    """ Product model for storing product data🛢 """
     product_name = models.CharField(max_length=100)
-    product_code = models.CharField(max_length=4, unique=True, default=random.unique_code(4))
+    product_code = models.CharField(max_length=4, unique=True, null=True, blank=True)
     product_model = models.CharField(max_length=100)
     product_description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     supplier_price = models.DecimalField(max_digits=10, decimal_places=2)
     unit = models.CharField(max_length=10, choices=[('kg', 'Kilogram'), ('l', 'Liter'), ('pcs', 'Pieces')])
-    image = models.ImageField(upload_to='product_images/')
-
+    image = CloudinaryField('Product Images', null=True, blank=True)
+    quantity = models.IntegerField(default=0)
+    count_sold = models.IntegerField(default=0)
+    out_of_stock = models.BooleanField(default=False)
+    in_stock = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, choices=[('out of stock', 'Out of Stock'), ('in stock', 'In Stock')], default='In stock')
+    recently_sold = models.DateTimeField(null=True, blank=True)
+    recently_added = models.DateTimeField(null=True, blank=True)
+    recently_viewed = models.DateTimeField(null=True, blank=True)
+    recently_updated = models.DateTimeField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+
+    history = HistoricalRecords()
+
+    def save(self, *args, **kwargs):
+        """ override the save method for logical purposes """
+        if self.in_stock != 0:
+            self.status = 'in stock'
+            self.out_of_stock = False
+        else:
+            self.status = 'out of stock'
+            # if out of stock, set out_of_stock flag to True
+            self.out_of_stock = True
+        # Save the product with a random product code.
+        self.product_code = random.unique_code(4)
+        super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
         """String for representing the Model object."""
