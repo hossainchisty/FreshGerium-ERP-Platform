7a9570f2d9b55a79466b019716520297f029db16
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-22 17:03:13 +0600

    ♻️ Improved random code generator

--- purchase/models.py
+++ purchase/models.py
@@ -8,11 +8,18 @@ class Purchase(Timestamp):
     """
     Purchase model for storing purchase data🛢
     """
-    invoice_number = models.CharField(max_length=4, unique=True, default=random.unique_code(4))
-    purchase_id = models.CharField(max_length=8, unique=True, default=random.unique_code(8))
+    invoice_number = models.CharField(max_length=4, unique=True)
+    purchase_id = models.CharField(max_length=8, unique=True)
     purchase_date = models.DateField()
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     supplier = models.ForeignKey(Supplier, on_delete=models.DO_NOTHING)
+
+    def save(self, *args, **kwargs):
+        """ override the save method for logical purposes """
+        # Save the purchase invoice_number, purchase_id with a random code.
+        self.invoice_number = random.unique_code(4)
+        self.purchase_id = random.unique_code(8)
+        super(Purchase, self).save(*args, **kwargs)
 
     def __str__(self):
         """String for representing the Model object."""
