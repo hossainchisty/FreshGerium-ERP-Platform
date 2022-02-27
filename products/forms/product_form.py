10ccc33e752f063e09c5eaebc34f4a12a1a08d11
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-02-27 15:18:33 +0600

    ✨ Added CK editor widget

--- products/forms/product_form.py
+++ products/forms/product_form.py
@@ -1,9 +1,14 @@
+from ckeditor.widgets import CKEditorWidget
+
+from django import forms
 from django.forms import ModelForm
 from products.models import Product
 
 
 class ProductForm(ModelForm):
     ''' Form asking for create products '''
+    product_description = forms.CharField(widget=CKEditorWidget())
+
     class Meta:
         model = Product
         fields = '__all__'
