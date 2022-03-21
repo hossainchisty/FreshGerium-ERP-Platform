ab186566a8af452a3cc37e36c2a4d7d671a27f01
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-21 19:04:31 +0600

    ✨ Update Sale Admin

--- sales/admin.py
+++ sales/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from sales.models import Sale
 
-from .models import Sale
 
-admin.site.register(Sale)
-admin.site.site_header = 'Freshdesk CRM Platform'
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = (
+        '_invoice_number', 'customer', 'product',
+        'date', 'payment_method', '_status', 'is_paid',
+        'total', 'due', 'total_profit',
+    )
+    list_filter = ('status', 'date', 'payment_method',  'product', 'customer',)
+    exclude = ('user', 'status', 'invoice_number')
+
+    def _status(self, obj):
+        '''
+        Return the status of the sale colorized in red or green depending on the status.
+        '''
+        return format_html('<span style="color:green">✅Paid</span>') if obj.is_paid else format_html('<span style="color:red">⌛Due</span>')
+
+    def _invoice_number(self, obj):
+        '''
+        Return the invoice_number colorized in admin.
+        '''
+        return format_html('<span style="color:green;">#{}</span>', obj.invoice_number)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
