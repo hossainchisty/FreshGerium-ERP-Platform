95307ce4a31886982bd88ccbda3f6c99ef08866c
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-21 19:09:34 +0600

    ⚡️ Added signals

--- expense/apps.py
+++ expense/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ExpenseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'expense'
+
+    def ready(self):
+        import expense.signals
