922fdccb6869eef873062f7008de1f488517b31f
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-22 13:18:53 +0600

    ✨ Added sales view and latest urls updates

--- sales/urls.py
+++ sales/urls.py
@@ -1,7 +1,20 @@
 from django.urls import include, path
-from sales.views import manage_sale_views as views
+from sales.views.add_sale_views import CreateSale
+from sales.views.delete_sale_views import DeleteSale
+from sales.views.download_sale_pdf import DownloadSalePDF
+from sales.views.export_sale_csv_views import DownloadSaleCSV
+from sales.views.manage_sale_views import SalesList
+from sales.views.sale_pdf_views import ViewSalePDF
+from sales.views.update_sale_views import UpdateSale
 
 urlpatterns = [
+    path('', SalesList.as_view(), name='sales_list'),
+    path('add/', CreateSale.as_view(), name='add_sales'),
+    path('update/<int:pk>/', UpdateSale.as_view(), name='update_sale'),
+    path('delete/<int:pk>/', DeleteSale.as_view(), name='delete_sale'),
+    path('pdf/download/', DownloadSalePDF.as_view(), name="download_sale_pdf"),
+    path('pdf/', ViewSalePDF.as_view(), name='view_sale_pdf'),
+    path('export/', DownloadSaleCSV.as_view(), name="download_sale_csv"),
+    # API URLS
     path('api/v1/', include('sales.api.urls.sales_urls')),
-    path('', views.SalesList.as_view(), name='sales_list'),
 ]
---
+++ sales/views/add_sale_views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import redirect, render
+from django.views import View
+from sales.forms.sale_form import SaleForm
+
+
+class CreateSale(View):
+    ''' This class is used to create a new sales. '''
+
+    def get(self, request, *args, **kwargs):
+        ''' Get the sales form '''
+        return render(request,  'sales/add_sales.html', {'forms': SaleForm()})
+
+    def post(self, request, *args, **kwargs):
+        ''' Create a new sales '''
+
+        form = SaleForm(request.POST)
+        # Automatically set to the currently logged-in user
+        form.instance.user = request.user
+        if form.is_valid():
+            form.save()
+            """Provide a redirect on GET request."""
+            return redirect('sales_list')
+        else:
+            return render(request, 'sales/add_sales.html', {'forms': form})
+        """Provide a redirect on GET request."""
+        return redirect('sales_list')
