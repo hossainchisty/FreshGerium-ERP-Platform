8ce3f8574854741962744577b4ab2ff61f0ca1b2
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-22 13:29:10 +0600

    ✨ Added pagination of sales lists

--- sales/views/manage_sale_views.py
+++ sales/views/manage_sale_views.py
@@ -1,18 +1,29 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.views.generic import View
 from sales.models import Sale
 
 
+@method_decorator(cache_page(60 * 2), name='dispatch')
 class SalesList(View):
     def get(self, request):
         ''' List of all sales '''
         sales_list = Sale.objects.all().order_by('-id')
-        paginator = Paginator(sales_list, 25)
+        paginator = Paginator(sales_list, 10)
         page_number = request.GET.get('page')
-        sales = paginator.get_page(page_number)
+
+        try:
+            page_object = paginator.page(page_number)
+        except PageNotAnInteger:
+            # if page is not an integer, deliver the first page
+            page_object = paginator.page(1)
+        except EmptyPage:
+            # if the page is out of range, deliver the last page
+            page_object = paginator.page(paginator.num_pages)
 
         context = {
-            'sales': sales
+            'sales': page_object
         }
         return render(request, 'sales/sales_list.html', context)
