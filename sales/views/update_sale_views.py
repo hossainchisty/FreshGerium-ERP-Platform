a425c6b84886854560a03d957ab94d52fa4d2701
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-22 12:45:30 +0600

    📝 Updated sale edit fields

--- sales/views/update_sale_views.py
+++ sales/views/update_sale_views.py
@@ -4,7 +4,8 @@ from sales.models import Sale
 
 
 class UpdateSale(UpdateView):
+    ''' This view is used to update the sale model. '''
     model = Sale
-    fields = '__all__'
+    fields = ['invoice_number', 'customer', 'product', 'date', 'payment_method', 'is_paid', 'discount', 'due', 'total']
     template_name = 'sales/sales_form.html'
     success_url = reverse_lazy('sales_list')
