4a6a47cef50041c70cea3ae30585de5a88b86ca6
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 14:46:59 +0600

    ✨ Update Purchase Admin v1.0

--- purchase/admin.py
+++ purchase/admin.py
@@ -2,4 +2,32 @@ from django.contrib import admin
 
 from .models import Purchase
 
-admin.site.register(Purchase)
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    date_hierarchy = 'purchase_date'
+    list_display = (
+        'product', 'supplier', 'invoice_number',
+        'purchase_id', 'purchase_date', 'payment_method',
+        'discount', 'paid_amount', 'due_amount', 'total_amount'
+    )
+    list_filter = ('purchase_date', 'payment_method')
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'product', 'supplier', 'purchase_date', 'payment_method', 'details',
+                       'discount', 'paid_amount', 'due_amount', 'total_amount')
+        }),
+    )
+    readonly_fields = ('invoice_number', 'purchase_id',)
+    search_fields = ('invoice_number', 'purchase_id',)
+    ordering = ('-purchase_date',)
+    list_per_page = 10
+    exclude = ('invoice_number', 'purchase_id',)
+    list_editable = ('paid_amount', 'due_amount',)
+
+    def save_model(self, request, obj, form, change):
+        '''
+        Associate model with current user while saving.
+        '''
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
