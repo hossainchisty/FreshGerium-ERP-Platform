14ff46388ded97e6b2253d59235378077c7f3894
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 14:46:26 +0600

    🐛 Fixed currently loggedi-in user saving

--- purchase/views/add_purchase_views.py
+++ purchase/views/add_purchase_views.py
@@ -4,18 +4,19 @@ from purchase.forms.purchase_form import PurchaseForm
 
 
 class CreatePurchase(View):
-    '''
-    Intentionally simple parent class for all views.
-    '''
+    ''' Create a new purchase '''
+
     def get(self, request, *args, **kwargs):
         return render(request,  'purchase/add_purchase.html', {'forms': PurchaseForm()})
 
     def post(self, request, *args, **kwargs):
         ''' Create a new purchase '''
         form = PurchaseForm(request.POST)
+        # Automatically set to the currently logged-in user
+        form.instance.user = request.user
         if form.is_valid():
             form.save()
-            """Provide a redirect on GET request."""
+            ''' Provide a redirect on GET request.'''
             return redirect('manage_purchase')
         else:
             return render(request, 'purchase/add_purchase.html', {'forms': form})
--- purchase/views/update_purchase_views.py
+++ purchase/views/update_purchase_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 from purchase.models import Purchase
@@ -8,3 +9,9 @@ class UpdatePurchase(UpdateView):
     fields = ['purchase_date', 'total_amount', 'supplier']
     template_name = 'purchase/purchase_form.html'
     success_url = reverse_lazy('manage_purchase')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied("You are not allowed to edit this purchase.")
+        return obj
