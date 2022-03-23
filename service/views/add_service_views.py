b46660aeb3cfaa0b220c7deb5f6f6b0b614695d0
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 15:08:19 +0600

    🚑 Add user filtering while add, update and delete object

--- service/views/add_service_views.py
+++ service/views/add_service_views.py
@@ -4,19 +4,19 @@ from service.models import Service
 
 
 class CreateService(View):
-    '''
-    Intentionally simple parent class for all views.
-    '''
+    ''' Create a new expense '''
+
     def get(self, request, *args, **kwargs):
+        ''' Respond to GET request '''
         return render(request,  'service/add_service.html')
 
     def post(self, request, *args, **kwargs):
-        ''' Create a new expense '''
+        ''' Respond to POST request '''
         service_name = request.POST.get('service_name')
         service_charge = request.POST.get('service_charge')
         description = request.POST.get('service_description')
         vat = request.POST.get('service_vat')
-        service = Service(service_name=service_name, charge=service_charge, description=description, vat=vat)
+        service = Service(user=request.user, service_name=service_name, charge=service_charge, description=description, vat=vat)
         service.save()
-        """Provide a redirect on GET request."""
+        '''Provide a redirect on GET request.'''
         return redirect('manage_service')
--- service/views/update_service_views.py
+++ service/views/update_service_views.py
@@ -1,4 +1,5 @@
 
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 from service.models import Service
@@ -9,3 +10,9 @@ class ServiceUpdate(UpdateView):
     fields = ['service_name', 'charge', 'description', 'vat']
     template_name = 'service/update_service_form.html'
     success_url = reverse_lazy('manage_service')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied("You are not allowed to edit this service.")
+        return obj
