33d6953f1e4f68f4cfc343c6ab206441d9b9f423
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 14:47:51 +0600

    ✨ Updated Purchase model

--- purchase/models.py
+++ purchase/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.timezone import now
+from products.models.product_model import Product
 from suppliers.models import Supplier
 from utils import random
 from utils.models.common_fields import Timestamp
@@ -8,11 +10,27 @@ class Purchase(Timestamp):
     """
     Purchase model for storing purchase data🛢
     """
-    invoice_number = models.CharField(max_length=4, unique=True)
-    purchase_id = models.CharField(max_length=8, unique=True)
-    purchase_date = models.DateField()
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
     supplier = models.ForeignKey(Supplier, on_delete=models.DO_NOTHING)
+    invoice_number = models.CharField(max_length=4, unique=True, null=True, blank=True)
+    purchase_id = models.CharField(max_length=8, unique=True, null=True, blank=True)
+    purchase_date = models.DateField(verbose_name='Purchase Date', default=now)
+    payment_options = (
+        ('cash payment', 'Cash Payment'),
+        ('bank payment', 'Bank Payment'),
+        ('online payment', 'Online Payment'),
+    )
+    payment_method = models.CharField(
+        verbose_name='Payment Type',
+        max_length=20,
+        choices=payment_options,
+        default='cash payment',
+    )
+    details = models.TextField(verbose_name='Details', null=True, blank=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    paid_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    due_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     def save(self, *args, **kwargs):
         """ override the save method for logical purposes """
