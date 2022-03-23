e4d38f4d108396b8ccd250bed1119809b0664f50
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 15:12:50 +0600

    🚑 Add user filtering while delete object

--- expense/views/delete_expense_views.py
+++ expense/views/delete_expense_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView
 from expense.models import Expense
@@ -6,3 +7,9 @@ from expense.models import Expense
 class DeleteExpense(DeleteView):
     model = Expense
     success_url = reverse_lazy('expense_item')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied("You are not allowed to delete this expense.")
+        return obj
