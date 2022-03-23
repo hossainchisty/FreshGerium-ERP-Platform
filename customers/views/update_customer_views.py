776796f6682962b944c6de1bce5aff74e7149fe0
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 14:53:12 +0600

    🐛 Fixed bug while update customer

--- customers/views/update_customer_views.py
+++ customers/views/update_customer_views.py
@@ -1,4 +1,5 @@
 from customers.models import Customer
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 
@@ -8,3 +9,9 @@ class UpdateCustomer(UpdateView):
     fields = ['customer_name', 'customer_email', 'customer_address', 'mobile_no', 'balance', 'previous_balance']
     template_name = 'customers/customer_form.html'
     success_url = reverse_lazy('customer_list')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied("You are not allowed to edit this customer.")
+        return obj
