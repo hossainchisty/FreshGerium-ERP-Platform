251301286793e31446a81df0ce7c28bd1c375668
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 15:10:38 +0600

    🚑 Add user filtering while delete object

--- sales/views/delete_sale_views.py
+++ sales/views/delete_sale_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView
 from sales.models import Sale
@@ -6,3 +7,9 @@ from sales.models import Sale
 class DeleteSale(DeleteView):
     model = Sale
     success_url = reverse_lazy('sales_list')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied("You are not allowed to delete this sale.")
+        return obj
