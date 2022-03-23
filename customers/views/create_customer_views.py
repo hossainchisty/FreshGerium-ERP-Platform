fa750e5b8e855e9e1f6ccf8573dbedce44f7ea14
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 15:18:56 +0600

    🚑 Add user filtering while create object

--- customers/views/create_customer_views.py
+++ customers/views/create_customer_views.py
@@ -5,20 +5,27 @@ from django.views import View
 
 
 class CreateCustomer(View):
-    '''
-    Intentionally simple parent class for all views.
-    '''
+    ''' Create a new customer '''
+
     def get(self, request, *args, **kwargs):
+        ''' Respond to GET request '''
         return render(request,  'customers/add_customer.html')
 
     def post(self, request, *args, **kwargs):
-        ''' Create a new customer '''
+        ''' Respond to POST request '''
         customer_name = request.POST.get('customer_name')
         customer_email = request.POST.get('customer_email')
         mobile_no = request.POST.get('mobile_no')
         customer_address = request.POST.get('customer_address')
         balance = request.POST.get('balance')
-        customer = Customer(customer_name=customer_name, customer_email=customer_email, mobile_no=mobile_no, customer_address=customer_address, balance=balance)
+        customer = Customer(
+            user=reqeust.user,
+            customer_name=customer_name,
+            customer_email=customer_email,
+            mobile_no=mobile_no,
+            customer_address=customer_address,
+            balance=balance
+        )
         customer.save()
-        """Provide a redirect on GET request."""
+        '''Provide a redirect on GET request.'''
         return redirect('customer_list')
