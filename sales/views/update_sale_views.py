db2437eb0e3db3abbeafd73a413f44ef77d7b126
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 15:11:32 +0600

    🚑 Add user filtering while update object

--- sales/views/update_sale_views.py
+++ sales/views/update_sale_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 from sales.models import Sale
@@ -9,3 +10,9 @@ class UpdateSale(UpdateView):
     fields = ['invoice_number', 'customer', 'product', 'date', 'payment_method', 'is_paid', 'discount', 'due', 'total']
     template_name = 'sales/sales_form.html'
     success_url = reverse_lazy('sales_list')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied("You are not allowed to edit this sale.")
+        return obj
