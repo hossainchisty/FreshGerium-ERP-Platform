6f2f9e7b0492a8509fb2d086135ec0e84457927a
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 15:17:12 +0600

    🚑 Add user filtering while update object

--- damage/views/update_damage_views.py
+++ damage/views/update_damage_views.py
@@ -1,4 +1,5 @@
 from damage.models import Damage
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 
@@ -8,3 +9,9 @@ class UpdateDamage(UpdateView):
     fields = '__all__'
     template_name = 'damage/damage_form.html'
     success_url = reverse_lazy('damage_list')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied("You are not allowed to edit this damage.")
+        return obj
