7a11b32df2caec06e2c596c0cd08bcc0497864db
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-03-23 14:50:00 +0600

    🐛 Fixed bug while update expense

--- expense/views/update_expense_views.py
+++ expense/views/update_expense_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 from expense.models import Expense
@@ -8,3 +9,9 @@ class UpdateExpense(UpdateView):
     fields = ['expense_type']
     template_name = 'expense/expense_form.html'
     success_url = reverse_lazy('expense_item')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied("You are not allowed to edit this expense.")
+        return obj
