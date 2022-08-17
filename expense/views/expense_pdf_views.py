37616e7773763ae89e44dc43bb0fef62d2921c01
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-08-17 14:27:01 +0600

    ♻️ Fix indentation block

--- expense/views/expense_pdf_views.py
+++ expense/views/expense_pdf_views.py
@@ -8,9 +8,9 @@ from utils.render_to_pdf import generate_pdf
 
 
 class ViewExpensePDF(LoginRequiredMixin, View):
-	''' This view will show pdf '''
-	@method_decorator(_currentUser())
-	def get(self, request, *args, **kwargs):
-		expense = Expense.objects.all().order_by('-id')
-		pdf = generate_pdf('expense/pdf/expense_pdf.html', {'expense': expense})
-		return HttpResponse(pdf, content_type='application/pdf')
+    ''' This view will show pdf '''
+    @method_decorator(_currentUser())
+    def get(self, request, *args, **kwargs):
+        expense = Expense.objects.all().order_by('-id')
+        pdf = generate_pdf('expense/pdf/expense_pdf.html', {'expense': expense})
+        return HttpResponse(pdf, content_type='application/pdf')
