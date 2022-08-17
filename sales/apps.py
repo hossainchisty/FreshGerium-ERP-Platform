27ed935c34704feecab2ab19b676d590ec38fa7c
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-08-17 18:27:40 +0600

    🐛 Added ready method

--- sales/apps.py
+++ sales/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SalesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sales'
+
+    def ready(self):
+        import sales.signals
