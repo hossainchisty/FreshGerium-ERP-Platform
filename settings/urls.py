e8af135948a7b2343f855e1c23abd6e6c1c7759d
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-08-22 13:25:10 +0600

    ✨ Added new featues

--- settings/urls.py
+++ settings/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from settings.views.delete_account_views import DeleteAccountView
 from settings.views.password_change_views import passwordChangeView
 from settings.views.price_plans_views import PricePlansView
 from settings.views.privacy_and_security_views import PrivacyAndSecurityView
@@ -9,4 +10,5 @@ urlpatterns = [
     path('plans/', PricePlansView.as_view(), name='price_plans'),
     path('password/change/', passwordChangeView, name='password_change'),
     path('accounts/privacy_and_security/', PrivacyAndSecurityView.as_view(), name='privacy_and_security'),
+    path('accounts/delete/', DeleteAccountView.as_view(), name='delete_account'),
 ]
