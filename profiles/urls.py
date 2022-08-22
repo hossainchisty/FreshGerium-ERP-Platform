2686ba6cc44d5d8270a824c4e8c2f9a6f408f660
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-08-22 13:22:07 +0600

    ✨ Added new method

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from profiles.views.data_download_views import data_download
+from profiles.views.data_views import data_view
 from profiles.views.download_confirm_views import download_confirm
 from profiles.views.download_requested_views import download_requested
 from profiles.views.password_verification_views import password_verification
@@ -9,6 +10,7 @@ urlpatterns = [
     path('', profile, name='profile'),
     path('download/request/', data_download, name='data_download'),
     path('verification/', password_verification, name='password_verification'),
-    path('download/requested/', download_requested, name='download_requested'),
+    path('download/requested/', download_requested, name='data_download_requested'),
     path('download/confirm/', download_confirm, name='download_confirm'),
+    path('data/', data_view, name='data_view'),
 ]
