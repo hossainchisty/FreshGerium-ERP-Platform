05a8c24278a0a1dfb8e36a3768bfc03365ba592f
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-08-28 17:42:12 +0600

    💩 Added multi-db router

--- database/routers/db_routers.py
+++ database/routers/db_routers.py
@@ -1,25 +1,25 @@
-class ExpenseRouter:
-    route_app_labels = {'expense'}
+# class ExpenseRouter:
+#     route_app_labels = {'expense'}
 
-    def db_for_read(self, model, **hints):
-        if model._meta.app_label in self.route_app_labels:
-            return 'expense_db'
-        return None
+#     def db_for_read(self, model, **hints):
+#         if model._meta.app_label in self.route_app_labels:
+#             return 'expense_db'
+#         return None
 
-    def db_for_write(self, model, **hints):
-        if model._meta.app_label in self.route_app_labels:
-            return 'expense_db'
-        return None
+#     def db_for_write(self, model, **hints):
+#         if model._meta.app_label in self.route_app_labels:
+#             return 'expense_db'
+#         return None
 
-    def allow_relation(self, obj1, obj2, **hints):
-        if (
-            obj1._meta.app_label in self.route_app_labels or
-            obj2._meta.app_label in self.route_app_labels
-        ):
-            return True
-        return None
+#     def allow_relation(self, obj1, obj2, **hints):
+#         if (
+#             obj1._meta.app_label in self.route_app_labels or
+#             obj2._meta.app_label in self.route_app_labels
+#         ):
+#             return True
+#         return None
 
-    def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label in self.route_app_labels:
-            return db == 'expense_db'
-        return None
+#     def allow_migrate(self, db, app_label, model_name=None, **hints):
+#         if app_label in self.route_app_labels:
+#             return db == 'expense_db'
+#         return None
