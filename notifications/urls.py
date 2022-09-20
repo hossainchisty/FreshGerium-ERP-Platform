0904d099aecf6ab00303db242045170fc5ba777b
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-09-20 20:23:32 +0600

    🔥 Removed files

--- notifications/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from notifications.views.notifications_views import NotificationView
-
-urlpatterns = [
-    path('', NotificationView.as_view(), name='notifications'),
-]
