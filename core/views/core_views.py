d85d25386ba38541a908037f9286e3adc0114746
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-11-27 17:42:10 +0600

    🚑 Added login required method

--- core/views/core_views.py
+++ core/views/core_views.py
@@ -1,9 +1,12 @@
 from authenticator.models import User
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.generic import View
 from products.models.product_model import Product
 
 
+@method_decorator(login_required, name='dispatch')
 class Dashboard(View):
     def get(self, request):
         ''''
