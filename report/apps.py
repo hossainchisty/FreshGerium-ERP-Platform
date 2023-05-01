a23b9c7790b977e9a20bbf83af4d6bc215dddd6a
hossainchisty <hossain.chisty11@gmail.com>
2023-05-01 12:52:41 +0600

    🔥 Remove reports

--- report/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReportConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'report'
-    icon = 'fa fa-bug'
--- report/urls.py
+++
@@ -1,2 +0,0 @@
-# flake8: noqa
-from django.urls import path
