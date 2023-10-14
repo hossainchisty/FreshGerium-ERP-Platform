ec4c9c11cf09ea8a19d3b612e9105f27afd71a43
hossainchisty <hossain.chisty11@gmail.com>
2023-10-14 18:52:16 +0600

    refactor: setting up for prod

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,15 +12,3 @@ from config.settings.base import *
 
 # Override base.py settings here
 
-# Postgresql database config
-# DATABASES = {
-
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'freshdesk',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
